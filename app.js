5aacf15c5d66ca1881fd471a25544fd06a500540
rjgaming27 <74172553+rjgaming27@users.noreply.github.com>
2020-11-09 12:41:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,17 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCqo0MzWyPxXl-CHJrOR-We5ZZk4_4JbfA",
+  authDomain: "rjgaming-6c27d.firebaseapp.com",
+  databaseURL: "https://rjgaming-6c27d.firebaseio.com",
+  projectId: "rjgaming-6c27d",
+  storageBucket: "rjgaming-6c27d.appspot.com",
+  messagingSenderId: "703391268884",
+  appId: "1:703391268884:web:435ad2ee57e1b6f3516701",
+  measurementId: "G-DT9XJ8JMRR"
 };
-
      
  
 ////Do Not Edit Below This
